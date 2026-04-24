266a1f4fe3a05feb6d904cd04b7825527461fe41
millieloisi <167239633+millieloisi@users.noreply.github.com>
2026-04-24 14:23:06 -0300

    Header con botones y barra de busqueda. Icono random par ainciar sesion con funcionalidad de que cuando se toca te manda a inciar sesios, y si no tenes cuenta te manda a crear una., formulario de registro con posibles campos a completar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,25 @@
+import { useNavigate } from "react-router-dom"
+
+function Login() {
+  const navigate = useNavigate()
+
+  return (
+    <div className="auth-container">
+      <h2>Iniciar Sesión</h2>
+
+      <input type="text" placeholder="Email o usuario" />
+      <input type="password" placeholder="Contraseña" />
+
+      <button className="main-btn">Ingresar</button>
+
+      <p>
+        ¿No tienes una cuenta?{" "}
+        <span onClick={() => navigate("/register")}>
+          Regístrate
+        </span>
+      </p>
+    </div>
+  )
+}
+
+export default Login

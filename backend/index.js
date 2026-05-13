8761455f0e7b9b6f3e2359002daff294b6461325
Iair1 <166546999+Iair1@users.noreply.github.com>
2026-05-13 08:43:25 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,27 @@ import "dotenv/config";
 /*
 import UsuariosRouter from ./routers/usuarios.router.js
 import AptitudesRouter from ./routers/aptitudes.router.js
-import tdrRouter from ./routers/tdr.router.js
+import TdrRouter from ./routers/tdr.router.js
 import HistorialRouter from ./routers/historial.router.js
 import MensajesRouter from ./routers/mensajes.router.js
 import SolicitudesRouter from ./routers/solicitudes.router.js
 import AplicacionesRouter from ./routers/aplicaciones.router.js
 import TrabajosRouter from ./routers/trabajos.router.js
+*/
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (__, res) => res.send("Bienvenido a laburAR"));
+
+/*
+app.use("/usuarios", UsuariosRouter);
+app.use("/aptitudes", AptitudesRouter);
+app.use("/tdr", TdrRouter);
+app.use("/historial", HistorialRouter);
+app.use("/mensajes", MensajesRouter);
+app.use("/solicitudes", SolicitudesRouter);
+app.use("/aplicaciones", AplicacionesRouter);
+app.use("/trabajos", TrabajosRouter);
 */

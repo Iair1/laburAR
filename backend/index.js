b195f14b841b5e90c943effa81fc1975aa795217
Iair1 <166546999+Iair1@users.noreply.github.com>
2026-05-13 08:39:24 -0300

    Update index.js

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import "dotenv/config";
+
+/*
+import UsuariosRouter from ./routers/usuarios.router.js
+import AptitudesRouter from ./routers/aptitudes.router.js
+import tdrRouter from ./routers/tdr.router.js
+import HistorialRouter from ./routers/historial.router.js
+import MensajesRouter from ./routers/mensajes.router.js
+import SolicitudesRouter from ./routers/solicitudes.router.js
+import AplicacionesRouter from ./routers/aplicaciones.router.js
+import TrabajosRouter from ./routers/trabajos.router.js
+*/

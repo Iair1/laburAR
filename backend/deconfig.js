5819c8c23f9616ff3dc331c6ad4da28fe6bfbcb8
Iair1 <166546999+Iair1@users.noreply.github.com>
2026-05-13 08:32:23 -0300

    m

---
+++ backend/deconfig.js
@@ -0,0 +1,9 @@
+const config = {
+    host: 'ep-purple-resonance-aq89vxq0-pooler.c-8.us-east-1.aws.neon.tech',
+    database: 'neondb',
+    user: 'neondb_owner',
+    password: 'npg_w7AYJuiR1xBa',
+    post: 5432,
+    ssl: true
+}
+export default config;

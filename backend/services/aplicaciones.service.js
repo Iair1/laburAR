99f2063f8f0cef8b789d03b641439d4242530e8e
Iair1 <166546999+Iair1@users.noreply.github.com>
2026-08-12 09:05:14 -0300

    FALTA EL MENSAJE SI NO SE PUDO

--- backend/services/aplicaciones.service.js
+++ backend/services/aplicaciones.service.js
@@ -6,9 +6,14 @@ async function subirAplicacion(id, solicitudid, periodo) {
     const client = new Client(config);
     try {
         await client.connect();
-        constmresult =  await client.query(`INSERT INTO aplicaciones (trabajadorid, solicitudid, periodo)
-            VALUES ($1, $2, $3)
-            RETURNING `, [id, solicitudid, periodo]);
+        const result =  await client.query(
+            `INSERT INTO aplicaciones (trabajadorid, solicitudid, periodo)
+            SELECT $1, $2, $3
+            WHERE EXISTS(
+            SELECT 1 FROM solicitudes
+            WHERE periodo[0] <= $4
+            AND periodo[1] >= $5
+            AND id = $2)`, [id, solicitudid, periodo, periodo[0], periodo[1]]);
         return result.rows[0];
     } catch (error) {
         throw error;
